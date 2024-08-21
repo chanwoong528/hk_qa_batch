e1d22eb3f1851591da520831d09844bc84e92ec6
문찬웅 <mooncw@hankookilbo.com>
2024-08-22 08:34:01 +0900

    console added to check

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const Redis = require('ioredis');
 
 async function backupAndFlushBullMQData() {
   const redis = new Redis();
-  const backupFile = path.join(__dirname, `../logs/bullmq_backup${new Date().toDateString()}.txt`);
+  const backupFile = path.join(__dirname, `../logs/bullmq_backup${curDateFileNameFormat()}.txt`);
   const writeStream = fs.createWriteStream(backupFile, { flags: 'a' });
 
   try {
@@ -58,15 +58,30 @@ async function backupAndFlushBullMQData() {
   }
 }
 
-const everyOneMin = '*/1 * * * *'
+const everyOneMin = '*/10 * * * * *'
 const everyMidNight = '59 23 * * *'
 
 cron.schedule(process.env.NODE_ENV === "dev"
   ? everyOneMin
   : everyMidNight,
   () => {
+    console.log("Cron started: ", new Date().toLocaleString())
     backupAndFlushBullMQData();
-  });
+  }, {
+  scheduled: true,
+  timezone: 'Asia/Seoul'
+});
+
+
+const curDateFileNameFormat = () => {
+  const now = new Date();
+  // const formattedDate = now.toISOString().slice(0, 16).replace(/:/g, '-').replace('T', '-');
+  // // Format the date and time in yyyy-mm-dd:hh-mm format
+  const formattedDate = now.toISOString().slice(0, 16).replace('T', ':');
+  return formattedDate
+}
+
+
 app.listen(4000, () => {
   console.log('Batch Server is running on port 4000');
 });
