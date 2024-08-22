ca185830e8a5ab1128097be9db85afd6a1a4e3c5
root <cksdnd004@naver.com>
2024-08-22 18:47:25 +0900

    log folder create if not exsit

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -6,6 +6,15 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+if [ ! -d "logs" ]; then
+  # If the folder does not exist, create it
+  mkdir logs
+  echo "Log folder created."
+else
+  # If the folder exists, do nothing
+  echo "Logs folder already exists."
+fi
+
 npm install
 
 pm2 stop hkqa-batch
